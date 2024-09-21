a637d1a1becc2dc1d533d9f4530fdb315fa1c4a1
rushikesh garad <garadrushikesh042@gmail.com>
2024-09-21 06:37:56 +0530

    adding some fun

---
+++ jqery1.js
@@ -0,0 +1,7 @@
+let ad = document.getElementById('.search-box')
+
+$(document).ready(function(){
+    $('.close').click(function(){
+        $('.search-box').css('opacity','0')
+    })
+})
--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-gsap.from('#slideImg',{
-    scale:1.2,
-    duration:5,
-    dealy:2
-})
+function slideani()
+{
+    gsap.from('#slideImg',{
+        scale:1.2,
+        duration:3,
+        dealy:2
+    })
+    gsap.from('.empty1 span',{
+        y:80,
+        opacity:0,
+        duration:3,
+        delay:0.5,
+        stagger:1
+    })
+}
+slideani()
+let a = document.getElementsByClassName('search-box')
+
+let b = document.getElementsByClassName('close')
+
+\
